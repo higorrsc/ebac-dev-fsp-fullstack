82015b8707dfaab815151276b2567a32866af523
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-03-09 21:11:24 -0300

    feat: add comments application

---
+++ backend/comments/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework import viewsets, status, permissions
+
+from user_profile.permissions import IsOwnerOrReadOnly
+
+from .models import Comment
+from .serializers import CommentSerializer
+
+
+# Create your views here.
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
