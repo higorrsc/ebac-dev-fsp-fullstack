570568e9415182a6f1f9a193c764754d1d8b355e
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-03-09 21:12:25 -0300

    feat: add votes application

---
+++ backend/votes/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404
+from rest_framework import serializers, viewsets, status, permissions
+
+from .models import Vote
+from .permissions import hasSelfVotedOrReadOnly
+from .serializers import VoteSerializer
+
+from posts.models import Post
+
+
+# Create your views here.
+class VoteViewSet(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, hasSelfVotedOrReadOnly]
+
+    def perform_create(self, serializer):
+        post_instance = get_object_or_404(Post, pk=self.request.data["post"])
+
+        # if user likes the post
+        if self.request.data["up_vote"]:
+            already_up_voted = Vote.objects.filter(
+                post=post_instance, up_vote_by=self.request.user
+            ).exists()
+            if already_up_voted:
+                raise serializers.ValidationError(
+                    {"message": "Você já deu like neste post!"}
+                )
+            else:
+                serializer.save(up_vote_by=self.request.user, post=post_instance)
+        # if dislikes
+        else:
+            already_down_voted = Vote.objects.filter(
+                post=post_instance, down_vote_by=self.request.user
+            ).exists()
+            if already_down_voted:
+                raise serializers.ValidationError(
+                    {"message": "Você já deu dislike neste post!"}
+                )
+            else:
+                serializer.save(down_vote_by=self.request.user, post=post_instance)
