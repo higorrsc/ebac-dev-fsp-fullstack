2165991e861c8633b5296ffdba5069e887558fc6
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-03-27 20:49:13 -0300

    feat: add eslint import order config

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Link from 'next/link'
+
 import { Button } from '@/components/ui/button'
 import {
     Card,
@@ -12,53 +14,67 @@ import {
 import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import Link from 'next/link'
 
 export default function LoginAccount() {
     return (
         <div className="relative flex flex-col justify-center items-center min-h-screen overflow-hidden">
             <div className="w-full m-auto lg:max-w-lg">
-                <Card>
-                    <CardHeader className="space-y-1">
-                        <CardTitle className="text-2xl text-center">
-                            Login
-                        </CardTitle>
-                        <CardDescription className="text-center">
-                            Informe seu usuário e sua senha
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="grid gap-4">
-                        <div className="grid gap-2">
-                            <Label htmlFor="username">Usuário</Label>
-                            <Input id="username" type="text" placeholder="" />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="password">Senha</Label>
-                            <Input id="password" type="password" />
-                        </div>
-                        <div className="flex items-center space-x-2">
-                            <Checkbox id="terms" />
-                            <label
-                                htmlFor="terms"
-                                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                <form id="login" onSubmit={(e) => e.preventDefault()}>
+                    <Card>
+                        <CardHeader className="space-y-1">
+                            <CardTitle className="text-2xl text-center">
+                                Login
+                            </CardTitle>
+                            <CardDescription className="text-center">
+                                Informe seu usuário e sua senha
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="grid gap-4">
+                            <div className="grid gap-2">
+                                <Label htmlFor="username">Usuário</Label>
+                                <Input
+                                    id="username"
+                                    type="text"
+                                    placeholder=""
+                                    autoComplete="username"
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="password">Senha</Label>
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                />
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <Checkbox id="terms" />
+                                <label
+                                    htmlFor="terms"
+                                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                >
+                                    Manter conectado
+                                </label>
+                            </div>
+                        </CardContent>
+                        <CardFooter className="flex flex-col">
+                            <Button
+                                className="w-full border rounded bg-slate-700"
+                                type="submit"
+                                onClick={() => console.log('login')}
                             >
-                                Manter conectado
-                            </label>
-                        </div>
-                    </CardContent>
-                    <CardFooter className="flex flex-col">
-                        <Button className="w-full border rounded bg-slate-700">
-                            Login
-                        </Button>
-                        <p className="mt-2 text-xs text-center text-gray-700">
-                            {' '}
-                            Não tem uma conta?{' '}
-                            <span className=" text-blue-600 hover:underline">
-                                <Link href={'/signup'}>Cadastre-se</Link>
-                            </span>
-                        </p>
-                    </CardFooter>
-                </Card>
+                                Login
+                            </Button>
+                            <p className="mt-2 text-xs text-center text-gray-700">
+                                {' '}
+                                Não tem uma conta?{' '}
+                                <span className=" text-blue-600 hover:underline">
+                                    <Link href={'/signup'}>Cadastre-se</Link>
+                                </span>
+                            </p>
+                        </CardFooter>
+                    </Card>
+                </form>
             </div>
         </div>
     )
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Link from 'next/link'
+
 import { Button } from '@/components/ui/button'
 import {
     Card,
@@ -11,60 +13,71 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import Link from 'next/link'
 
 export default function Signup() {
     return (
         <div className="relative flex flex-col justify-center items-center min-h-screen overflow-hidden">
             <div className="w-full m-auto lg:max-w-lg">
-                <Card>
-                    <CardHeader className="space-y-1">
-                        <CardTitle className="text-2xl text-center">
-                            Cadastro
-                        </CardTitle>
-                        <CardDescription className="text-center">
-                            Faça seu cadastro na rede social
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="grid gap-4">
-                        <div>
-                            <Label htmlFor="email">E-Mail</Label>
-                            <Input id="email" type="email" placeholder="" />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="username">Usuário</Label>
-                            <Input id="username" type="text" placeholder="" />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="password">Senha</Label>
-                            <Input id="password" type="password" />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="password">Confirme sua Senha</Label>
-                            <Input id="confirmPassword" type="password" />
-                        </div>
-                    </CardContent>
-                    <CardFooter className="flex flex-col">
-                        <Button className="w-full border rounded bg-slate-700">
-                            Cadastrar
-                        </Button>
-                        <p className="text-center text-sm mt-16">
-                            Ao se inscrever, você concorda com os Termos de
-                            Serviço e a Política de Privacidade, incluindo o Uso
-                            de Cookies.
-                        </p>
+                <form id="signup" onSubmit={(e) => e.preventDefault()}>
+                    <Card>
+                        <CardHeader className="space-y-1">
+                            <CardTitle className="text-2xl text-center">
+                                Cadastro
+                            </CardTitle>
+                            <CardDescription className="text-center">
+                                Faça seu cadastro na rede social
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="grid gap-4">
+                            <div>
+                                <Label htmlFor="email">E-Mail</Label>
+                                <Input id="email" type="email" placeholder="" />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="username">Usuário</Label>
+                                <Input
+                                    id="username"
+                                    type="text"
+                                    placeholder=""
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="password">Senha</Label>
+                                <Input id="password" type="password" />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="password">
+                                    Confirme sua Senha
+                                </Label>
+                                <Input id="confirmPassword" type="password" />
+                            </div>
+                        </CardContent>
+                        <CardFooter className="flex flex-col">
+                            <Button
+                                className="w-full border rounded bg-slate-700"
+                                type="submit"
+                                onClick={() => console.log('signup')}
+                            >
+                                Cadastrar
+                            </Button>
+                            <p className="text-center text-sm mt-16">
+                                Ao se inscrever, você concorda com os Termos de
+                                Serviço e a Política de Privacidade, incluindo o
+                                Uso de Cookies.
+                            </p>
 
-                        <p className="text-center  mt-8 mb-4">
-                            Já possui uma conta?
-                        </p>
-                        <Link
-                            href={'/login'}
-                            className="border-1 w-full rounded bg-slate-700 h-10 flex items-center justify-center"
-                        >
-                            Entrar
-                        </Link>
-                    </CardFooter>
-                </Card>
+                            <p className="text-center  mt-8 mb-4">
+                                Já possui uma conta?
+                            </p>
+                            <Link
+                                href={'/login'}
+                                className="border-1 w-full rounded bg-slate-700 h-10 flex items-center justify-center"
+                            >
+                                Entrar
+                            </Link>
+                        </CardFooter>
+                    </Card>
+                </form>
             </div>
         </div>
     )
