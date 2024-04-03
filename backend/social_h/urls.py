5c67207a5adb12b1b1dc21ede8a85e85dbaa0d4d
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-04-02 22:00:58 -0300

    feat: add urls for hello world and update server

--- backend/social_h/urls.py
+++ backend/social_h/urls.py
@@ -25,12 +25,16 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from . import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api-auth/", include("rest_framework.urls")),
+    path("update_server/", views.update, name="update"),
+    path("hello/", views.hello_world, name="hello_world"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
