79d1db593916a4b24e3a9c8cdf0c83b78feaeb44
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-04-07 14:50:19 -0300

    feat: add api service configuration for GET and POST

---
+++ frontend/app/services/apiService.ts
@@ -0,0 +1,39 @@
+const apiService = {
+    get: async function (url: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then((response) => response.json())
+                .then((json) => {
+                    resolve(json)
+                })
+                .catch((error) => reject(error))
+        })
+    },
+
+    post: async function (url: string, data: any): Promise<any> {
+        console.log('post', url, data)
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then((response) => response.json())
+                .then((json) => {
+                    resolve(json)
+                })
+                .catch((error) => reject(error))
+        })
+    }
+}
+
+export default apiService
