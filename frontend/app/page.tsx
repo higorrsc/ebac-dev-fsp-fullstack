f0f0e8dd43c83f3cfa50a4456223ed655f13f1e4
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-04-07 18:11:27 -0300

    feat: add logout button with avatar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import LogoutButton from '@/components/logout-button'
+import { getUserId } from '@/app/lib/actions'
+
+const Home = async () => {
+    const userId = await getUserId()
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            {userId && <LogoutButton />}
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </main>
     )
 }
+
+export default Home
