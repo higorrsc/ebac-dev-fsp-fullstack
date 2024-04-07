fe395a3bf484798b8f88ec57754443dadbbb84e3
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-04-07 18:11:54 -0300

    feat: add logout button component

---
+++ frontend/components/logout-button.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import { resetAuthCookies } from '@/app/lib/actions'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+
+const LogoutButton = () => {
+    const router = useRouter()
+
+    const submitLogout = async () => {
+        await resetAuthCookies()
+        router.push('/')
+    }
+
+    return (
+        <Button onClick={submitLogout}>
+            <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+        </Button>
+    )
+}
+
+export default LogoutButton
