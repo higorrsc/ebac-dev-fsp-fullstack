dc427d6597121bb8b783acdd2669fbf5bc809812
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-04-07 15:49:14 -0300

    feat: add shadcn toast component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/toaster'
 import { ToggleTheme } from '@/components/toggle-theme'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -30,6 +31,7 @@ export default function RootLayout({
                         <ToggleTheme />
                     </div>
                     {children}
+                    <Toaster />
                 </ThemeProvider>
             </body>
         </html>
