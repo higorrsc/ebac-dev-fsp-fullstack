1d1254633ce7041dd8bae175ab24ac39531921cc
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-04-08 21:36:37 -0300

    feat: change default page as a client component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,9 @@
-import LogoutButton from '@/components/logout-button'
-import { getUserId } from '@/lib/actions'
+'use client'
 
-const Home = async () => {
-    const userId = await getUserId()
-
+export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            {userId && <LogoutButton />}
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </main>
     )
 }
-
-export default Home
