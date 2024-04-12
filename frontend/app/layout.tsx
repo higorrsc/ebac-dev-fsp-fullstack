3ece051695354d69e79e0d86dc28d628361b0d06
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-04-11 22:15:10 -0300

    feat: add footer to default layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
 import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +30,7 @@ export default function RootLayout({
                 >
                     <Navbar />
                     {children}
+                    <Footer />
                     <Toaster />
                 </ThemeProvider>
             </body>
