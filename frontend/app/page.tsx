fc900b993d0d39ce6dec9f2d919f635a424090f0
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-04-11 22:14:51 -0300

    style: add some text to test de component position

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
+                quas minus doloribus, magnam delectus ab, nihil autem a
+                perspiciatis facilis distinctio cumque nisi provident id
+                mollitia, eligendi atque. Voluptatum, unde!
+            </p>
         </main>
     )
 }
