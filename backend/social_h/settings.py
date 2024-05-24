8d08cd5dd26b76cc9ae5bb82180a27479b4d628e
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-05-24 20:27:35 -0300

    feat: add environment variables to backend

--- backend/social_h/settings.py
+++ backend/social_h/settings.py
@@ -13,8 +13,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 
 from datetime import timedelta
-from decouple import config
+from dotenv import load_dotenv
 from pathlib import Path
+
+# Load environment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,23 +146,19 @@ WSGI_APPLICATION = "social_h.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
     # "default": {
-    #     "ENGINE": "django_cockroachdb",
-    #     "NAME": config("DATABASE_NAME"),
-    #     "USER": config("DATABASE_USER"),
-    #     "PASSWORD": config("DATABASE_PASS"),
-    #     "HOST": config("DATABASE_HOST"),
-    #     "PORT": config("DATABASE_PORT"),
-    #     "OPTIONS": {
-    #         "sslmode": "verify-full",
-    #     },
-    # },
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
