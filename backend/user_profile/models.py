b552ef7f685bec597b785db0cd12b4a380792af4
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-05-31 19:56:10 -0300

    feat: change first and last name required to false

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -8,8 +8,8 @@ class UserProfile(models.Model):
     owner = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name="profile_data"
     )
-    first_name = models.CharField(null=False, blank=False, max_length=150)
-    last_name = models.CharField(null=False, blank=False, max_length=150)
+    first_name = models.CharField(max_length=150, null=True, blank=True)
+    last_name = models.CharField(max_length=150, null=True, blank=True)
     gender = models.CharField(
         max_length=20,
         choices=options,
