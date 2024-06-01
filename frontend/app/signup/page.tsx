976b25761f17a64ba9f702766de48655f26e264d
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-01 17:45:05 -0300

    feat: add new signup component

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -2,24 +2,16 @@
 
 import Link from 'next/link'
 import * as z from 'zod'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import { Button } from '@/components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { ToastAction } from '@/components/ui/toast'
 import { useToast } from '@/components/ui/use-toast'
 import apiService from '@/app/services/apiService'
+import { getUserId } from '@/lib/actions'
 
 const registerUserSchema = z
   .object({
@@ -34,14 +26,9 @@ const registerUserSchema = z
       .string()
       .min(6, { message: 'A senha deve ter no mínimo 6 caracteres' })
   })
-  .superRefine(({ password, confirmPassword }, ctx) => {
-    if (password !== confirmPassword) {
-      ctx.addIssue({
-        code: 'custom',
-        message: 'As senhas devem ser iguais',
-        path: ['confirmPassword']
-      })
-    }
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: 'As senhas devem ser iguais',
+    path: ['confirmPassword']
   })
 
 type FormData = z.infer<typeof registerUserSchema>
@@ -49,6 +36,20 @@ type FormData = z.infer<typeof registerUserSchema>
 export default function SignUp() {
   const { toast } = useToast()
   const router = useRouter()
+
+  const [userId, setUserId] = useState<string | null>('')
+  useEffect(() => {
+    ;(async () => {
+      const gUserId = await getUserId()
+      setUserId(gUserId)
+    })()
+  })
+
+  useEffect(() => {
+    if (userId) {
+      router.push('/')
+    }
+  })
 
   const toastError = (description: string) => {
     toast({
@@ -59,27 +60,32 @@ export default function SignUp() {
   }
 
   const submitSignUp = async (data: FormData) => {
-    const apiData = {
+    const userData = {
       email: data.email,
       username: data.username,
       password: data.password
     }
-    const response = await apiService.post('/users/', apiData)
-    if (response.id) {
+    const res = await apiService.post('/users/', userData)
+    const errors = res.errors
+
+    if (errors) {
+      toastError(errors.join(', '))
+      return
+    }
+
+    if (res.id) {
       toast({
         description:
-          'Conta criada com sucesso! Você pode fazer o login e completar o seu perfil...',
+          'Conta criada com sucesso! Você pode fazer o login e completar seu perfil...',
         title: 'Sucesso',
         action: (
           <ToastAction altText="Ir para o login">
-            <Button onClick={() => router.push('/login')}>
-              Ir para o login
-            </Button>
+            <Button onClick={() => router.push('/login')}>Ir para login</Button>
           </ToastAction>
         )
       })
     } else {
-      if (!response.id && response.username) {
+      if (!res.id && res.username) {
         toastError('Nome de usuário em uso')
       }
     }
@@ -95,90 +101,112 @@ export default function SignUp() {
   })
 
   return (
-    <div className="relative flex flex-col items-center min-h-screen overflow-hidden">
-      <div className="w-full m-auto lg:max-w-lg">
-        <form id="signup" onSubmit={handleSubmit(submitSignUp)}>
-          <Card>
-            <CardHeader className="space-y-1">
-              <CardTitle className="text-2xl text-center">Cadastro</CardTitle>
-              <CardDescription className="text-center">
-                Faça seu cadastro na rede social
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-4">
-              <div>
-                <Label htmlFor="email">E-Mail</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder=""
-                  autoComplete="email"
-                  {...register('email')}
-                />
-                {errors.email && (
-                  <p className="text-red-500">{errors.email?.message}</p>
-                )}
+    <div className="container m-auto">
+      <div className="h-screen flex items-center justify-center">
+        <div
+          id="card"
+          className="w-full min-h-[600px] grid grid-cols-1 md:grid-cols-2 bg-slate-300 rounded-2xl overflow-hidden"
+        >
+          <div
+            id="left"
+            className="p-12 flex justify-center flex-col text-black"
+          >
+            <h1 className="text-2xl font-bold text-gray-700 pb-10">
+              Registre-se
+            </h1>
+            <form
+              id="signup"
+              onSubmit={handleSubmit(submitSignUp)}
+              className="flex flex-col gap-4"
+            >
+              <input
+                type="email"
+                id="email"
+                placeholder="E-mail"
+                className="border-b-2 outline-none bg-transparent p-2 w-full"
+                {...register('email')}
+              />
+              {errors.email && (
+                <p className="text-red-500 text-xs">{errors.email?.message}</p>
+              )}
+              <input
+                type="text"
+                id="username"
+                placeholder="Nome de usuário"
+                className="border-b-2 outline-none bg-transparent p-2 w-full"
+                {...register('username')}
+              />
+              {errors.username && (
+                <p className="text-red-500 text-xs">
+                  {errors.username?.message}
+                </p>
+              )}
+              <div className="grid grid-cols-2">
+                <div className="block">
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder="Senha"
+                    className="border-b-2 outline-none bg-transparent p-2 w-full"
+                    {...register('password')}
+                  />
+                  {errors.password && (
+                    <p className="text-red-500 text-xs">
+                      {errors.password?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="block">
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    placeholder="Confirmar a senha"
+                    className="border-b-2 outline-none bg-transparent p-2 w-full"
+                    {...register('confirmPassword')}
+                  />
+                  {errors.confirmPassword && (
+                    <p className="text-red-500 text-xs">
+                      {errors.confirmPassword?.message}
+                    </p>
+                  )}
+                </div>
               </div>
-              <div className="grid gap-2">
-                <Label htmlFor="username">Usuário</Label>
-                <Input
-                  id="username"
-                  type="text"
-                  placeholder=""
-                  {...register('username')}
-                />
-                {errors.username && (
-                  <p className="text-red-500">{errors.username?.message}</p>
-                )}
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Senha</Label>
-                <Input
-                  id="password"
-                  type="password"
-                  autoComplete="new-password"
-                  {...register('password')}
-                />
-                {errors.password && (
-                  <p className="text-red-500">{errors.password?.message}</p>
-                )}
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Confirme sua Senha</Label>
-                <Input
-                  id="confirmPassword"
-                  type="password"
-                  autoComplete="new-password"
-                  {...register('confirmPassword')}
-                />
-                {errors.confirmPassword && (
-                  <p className="text-red-500">
-                    {errors.confirmPassword?.message}
-                  </p>
-                )}
-              </div>
-            </CardContent>
-            <CardFooter className="flex flex-col">
-              <Button
-                className="w-full border rounded bg-slate-700"
+              <button
+                className="w-[50%] text-center border-2 bg-white border-black rounded-2xl p-2 text-sm"
                 type="submit"
               >
-                Cadastrar
-              </Button>
-              <p className="text-center text-sm mt-16">
-                Ao se inscrever, você concorda com os Termos de Serviço e a
-                Política de Privacidade, incluindo o Uso de Cookies.
-              </p>
-              <p className="text-center  mt-8 mb-4">Já possui uma conta?</p>
-              <Link
-                href={'/login'}
-                className="border-1 w-full rounded bg-slate-700 h-10 flex items-center justify-center"
-              >
-                Entrar
-              </Link>
-            </CardFooter>
-          </Card>
-        </form>
+                Registrar
+              </button>
+            </form>
+          </div>
+          <div
+            id="right"
+            className='bg-[linear-gradient(to_right_bottom,rgba(255,255,255,0.8),rgba(133,133,133,0.8)),url("https://cdn.pixabay.com/photo/2018/11/29/21/51/social-media-3846597_1280.png")] bg-cover bg-center p-12 flex flex-col gap-8 text-black'
+          >
+            <h1 className="text-6xl font-bold">Social H</h1>
+            <p className="text-sm">
+              Estamos felizes em apresentar a você uma nova rede social que está
+              sendo desenvolvida com o objetivo de conectar pessoas com
+              interesses em comum, compartilhar experiências, promover debates e
+              compartilhar conhecimento.
+            </p>
+            <p className="text-xs">O que você pode esperar da Social H?</p>
+            <ul className="list-disc list-inside text-xs">
+              <li>Um ambiente acolhedor e inclusivo</li>
+              <li>Ferramentas para facilitar a conexão</li>
+              <li>Conteúdos de qualidade</li>
+              <li>Oportunidade para se expressar</li>
+              <li>Um espaço para fazer a diferença</li>
+            </ul>
+            <span className="text-xs">Já tem uma conta?</span>
+            <Link
+              href="/login"
+              className="w-[50%] text-center border-2 bg-white border-black rounded-2xl p-2 text-sm"
+            >
+              Fazer login
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   )
