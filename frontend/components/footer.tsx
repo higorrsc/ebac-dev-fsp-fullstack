2de57f8efa54f636f994508118f961aa9839984f
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-05-31 21:15:52 -0300

    feat: add hidden class in small screens for footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -9,14 +9,13 @@ export default function Footer() {
   const links = [
     { title: 'Condições para o uso', path: '#' },
     { title: 'Política de Privacidade', path: '#' },
-    { title: 'Anúncios baseados em interesses', path: '#' },
     { title: 'Proteção de dados', path: '#' },
     { title: 'Acordo de Licença', path: '#' },
     { title: '©2023', path: '' }
   ]
   return (
-    <footer className=" w-full border-t border-border/40">
-      <ul className="flex justify-center text-sm">
+    <footer className="w-full lg:border-t lg:border-border/40">
+      <ul className="hidden lg:flex justify-center text-sm">
         {links.map((item, idx) => (
           <li key={idx + 200} className="p-5 py-0">
             <Link href={item.path} key={idx + 200} legacyBehavior passHref>
