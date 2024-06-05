d93bc4e644686b94ee2b2b69424048d4b0fbaf3c
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-04 22:21:06 -0300

    feat: add postbar component to main page

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -1,15 +1,11 @@
 'use client'
 
+import PostBar from '@/components/postbar'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-10">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum quas
-        minus doloribus, magnam delectus ab, nihil autem a perspiciatis facilis
-        distinctio cumque nisi provident id mollitia, eligendi atque.
-        Voluptatum, unde!
-      </p>
+    <main className="flex min-h-screen flex-col items-center p-4">
+      <PostBar />
     </main>
   )
 }
