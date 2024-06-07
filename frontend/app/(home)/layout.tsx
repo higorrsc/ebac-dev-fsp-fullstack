3a1d48e09b6e0cd90b6b718c51a1aa24d1aa4d9f
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-07 19:50:28 -0300

    feat: add footer component to default layout

--- frontend/app/(home)/layout.tsx
+++ frontend/app/(home)/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
             {children}
             <Sidebar />
           </div>
+          <Footer />
           <Toaster />
         </ThemeProvider>
       </body>
