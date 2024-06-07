4bf7e451ce9939669a52cfb6ba013a673d581805
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-07 19:21:35 -0300

    feat: add z-index class to navbar

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -12,7 +12,7 @@ import defaultUserImage from '@/images/profile/default-user.png'
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 flex h-14 items-center justify-between border-b-2 border-b-slate-500 bg-background/95 px-2 py-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <nav className="sticky top-0 z-50 flex h-14 items-center justify-between border-b-2 border-b-slate-500 bg-background/95 px-2 py-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div id="left" className="flex items-center gap-8">
         <Link href="/">
           <span className="text-xl font-bold text-slate-700 dark:text-slate-300">
