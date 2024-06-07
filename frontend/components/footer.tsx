5e3d678104ebe39ea26c524efee12986235b8ae9
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-07 19:50:47 -0300

    feat: add function to get current year

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,24 +1,24 @@
 'use client'
 
-import { useState } from 'react'
 import Link from 'next/link'
 
 export default function Footer() {
-  const [state, setState] = useState(false)
+  const currentYear = new Date().getFullYear()
 
   const links = [
     { title: 'Condições para o uso', path: '#' },
     { title: 'Política de Privacidade', path: '#' },
     { title: 'Proteção de dados', path: '#' },
     { title: 'Acordo de Licença', path: '#' },
-    { title: '©2023', path: '' }
+    { title: `©${currentYear}`, path: '#' }
   ]
+
   return (
     <footer className="w-full lg:border-t lg:border-border/40">
       <ul className="hidden justify-center text-sm lg:flex">
         {links.map((item, idx) => (
-          <li key={idx + 200} className="p-5 py-0">
-            <Link href={item.path} key={idx + 200} legacyBehavior passHref>
+          <li key={idx} className="p-5 py-0">
+            <Link href={item.path} legacyBehavior passHref>
               {item.title}
             </Link>
           </li>
