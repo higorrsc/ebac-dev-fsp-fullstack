4fdd7695ef38888405db799016aa64ad7a26d0d2
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-11 22:36:44 -0300

    feat: change responsive layout and html tag semantic

--- frontend/app/(home)/layout.tsx
+++ frontend/app/(home)/layout.tsx
@@ -3,9 +3,9 @@ import { Inter } from 'next/font/google'
 
 import '@/app/globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
-import Sidebar from '@/components/sidebar'
-import Footer from '@/components/footer'
+import Header from '@/components/header'
 import MenuBar from '@/components/menubar'
+import SideBar from '@/components/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,12 +28,27 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="container flex md:justify-around">
-            <MenuBar />
-            {children}
-            <Sidebar />
+          <div className="relative flex h-screen w-full overflow-hidden sm:container">
+            {/* <!-- Sidebar --> */}
+            <div className="flex w-12 flex-col sm:w-64">
+              <MenuBar />
+            </div>
+
+            <div className="flex h-full w-full flex-col justify-between">
+              {/* <!-- Header --> */}
+              <Header />
+
+              {/* <!-- Main --> */}
+              <main className="relative flex h-full max-w-full overflow-y-hidden">
+                {/* <!-- Container --> */}
+                <div className="no-scrollbar flex h-full w-full auto-cols-max grid-flow-col flex-wrap items-start justify-start gap-4 overflow-y-scroll rounded-tl">
+                  {/* <!-- Container --> */}
+                  {children}
+                </div>
+                <SideBar />
+              </main>
+            </div>
           </div>
-          <Footer />
         </ThemeProvider>
       </body>
     </html>
