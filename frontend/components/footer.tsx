ec6e4de7a78444708b1ebff97bede05f6f1c7145
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-11 22:36:09 -0300

    style: fix component position

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -14,8 +14,8 @@ export default function Footer() {
   ]
 
   return (
-    <footer className="w-full lg:border-t lg:border-border/40">
-      <ul className="hidden justify-center text-sm lg:flex">
+    <footer className="fixed bottom-0 w-full bg-background/95 p-2 backdrop-blur supports-[backdrop-filter]:bg-background/60 lg:border-t lg:border-border/40">
+      <ul className="hidden justify-center text-sm lg:flex lg:flex-wrap">
         {links.map((item, idx) => (
           <li key={idx} className="p-5 py-0">
             <Link href={item.path} legacyBehavior passHref>
