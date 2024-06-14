817e79d354fa552fc8caec5aa949e180bffe593b
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-13 21:37:22 -0300

    feat: remove unnecessary views

--- backend/social_h/urls.py
+++ backend/social_h/urls.py
@@ -33,7 +33,6 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api-auth/", include("rest_framework.urls")),
-    path("update_server/", views.update, name="update"),
     path("hello/", views.hello_world, name="hello_world"),
 ]
 
