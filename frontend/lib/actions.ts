1e4564244c2e8abbfe2744423b6ca915f1730465
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-14 23:22:53 -0300

    feat: remove user id and user name from functions and add jwt decode to get user id info

--- frontend/lib/actions.ts
+++ frontend/lib/actions.ts
@@ -1,25 +1,15 @@
 'use server'
 
 import { cookies } from 'next/headers'
+import { jwtDecode, JwtPayload } from 'jwt-decode'
 
-export async function handleLogin(
-  userId: string,
-  userName: string,
-  accessToken: string,
-  refreshToken: string
-) {
-  cookies().set('session_user_id', userId, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 60 * 60 * 24 * 7,
-    path: '/'
-  })
-  cookies().set('session_user_name', userName, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 60 * 60 * 24 * 7,
-    path: '/'
-  })
+import apiService from '@/app/services/apiService'
+
+interface ExtendedJwtPayload extends JwtPayload {
+  user_id: string
+}
+
+export async function handleLogin(accessToken: string, refreshToken: string) {
   cookies().set('session_access_token', accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
@@ -29,24 +19,54 @@ export async function handleLogin(
   cookies().set('session_refresh_token', refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    maxAge: 60 * 60,
+    maxAge: 60 * 60 * 24 * 7,
     path: '/'
   })
 }
 
 export async function resetAuthCookies() {
-  cookies().set('session_user_id', '')
-  cookies().set('session_user_name', '')
-  cookies().set('session_access_token', '')
-  cookies().set('session_refresh_token', '')
+  cookies().delete('session_access_token')
+  cookies().delete('session_refresh_token')
 }
 
 export async function getUserId() {
-  const userId = cookies().get('session_user_id')?.value
+  const token = cookies().get('session_access_token')?.value
+  if (!token) return null
+
+  const payload = jwtDecode<ExtendedJwtPayload>(token)
+  const userId = payload.user_id
   return userId ? userId : null
 }
 
-export async function getUserName() {
-  const userName = cookies().get('session_user_name')?.value
-  return userName ? userName : null
+export async function revalidateAccessToken() {
+  const token = cookies().get('session_access_token')?.value
+  if (!token) return null
+
+  const payload = jwtDecode<ExtendedJwtPayload>(token)
+  const currentTimestamp = Math.floor(Date.now() / 1000) // Current time in Unix time
+  const tokenExpiration = payload.exp
+
+  if (currentTimestamp >= tokenExpiration) {
+    const data = {
+      refresh: cookies().get('session_refresh_token')?.value
+    }
+    if (!data) return null
+
+    const response = await apiService.post('/token/refresh/', data)
+    const errors = response.errors
+
+    if (errors) return errors
+
+    if (response.access && response.refresh) {
+      handleLogin(response.access, response.refresh)
+    }
+  }
+
+  return
+}
+
+export async function getAccessToken() {
+  let accessToken = cookies().get('session_access_token')?.value
+
+  return accessToken
 }
