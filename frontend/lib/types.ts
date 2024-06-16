2188880deb1e0c7b8659c08430b391d3895cb1a4
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-15 22:50:57 -0300

    feat: add post, comment and vote types

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -19,3 +19,30 @@ export type Profile = {
   studies_at: string | null
   profile_image: string | null
 }
+
+export type Post = {
+  id: number
+  owner: number
+  content: string
+  post_image: string | null
+  category: string | null
+  post_date: string
+  comments?: Comment[]
+  votes?: Vote[]
+}
+
+export type Comment = {
+  id: number
+  comment: string
+  comment_image: string | null
+  comment_date: string
+  commented_by: string
+  post: number
+}
+
+export type Vote = {
+  id: number
+  post: number
+  up_vote_by?: string
+  down_vote_by?: string
+}
