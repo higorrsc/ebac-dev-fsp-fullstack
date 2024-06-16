62be6f327575a4901afa2cd298d5d7ac9339f928
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-15 22:30:37 -0300

    feat: remove custom fields

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,9 +11,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 # Create your views here.
 class UserLogout(APIView):
     def get(self, request):
-        # Directly logs out the user who made the request and deletes their session.
         logout(request)
-        # Return success response
         return Response({"detail": "Logout Successful"}, status=status.HTTP_200_OK)
 
 
@@ -35,20 +33,10 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-
-        # Add custom claims
-        token["custom_field"] = "Custom value"
-
         return token
 
     def validate(self, attrs):
         data = super().validate(attrs)
-
-        user = self.user
-        # data["user_id"] = user.id
-        # data["username"] = user.username
-        # ... add other user information as needed
-
         return data
 
 
