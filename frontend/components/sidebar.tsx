e3e67e62bf0d32cc84bc6113cf7be4c4e4a4bca2
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-15 22:56:53 -0300

    feat: refactor the code and remove modal message component

--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -4,110 +4,76 @@ import { useEffect, useState } from 'react'
 
 import apiService from '@/app/services/apiService'
 import InfoBlock from '@/components/infoblock'
-import ModalMessage from '@/components/modalmessage'
 import { UserCard } from '@/components/usercard'
 import defaultUser from '@/images/profile/default-user.png'
 import { getUserId } from '@/lib/actions'
 import { User } from '@/lib/types'
 
 export default function SideBar() {
+  const [usersProfile, setUsersProfile] = useState<User[] | null>(null)
   const [userId, setUserId] = useState<number | null>(null)
-  const [usersProfile, setUsersProfile] = useState<User[]>()
-  const [modalDescription, setModalDescription] = useState('')
-  const [isOpen, setIsOpen] = useState(false)
 
   useEffect(() => {
-    const fetchUserId = async () => {
+    const fetchData = async () => {
       setUserId(await getUserId())
-    }
-    fetchUserId()
-
-    if (userId) {
-      const fetchProfileData = async () => {
+      if (userId) {
         const response = await apiService.getWithAuth(
           `/friendship/find_friends/`
         )
         const errors = response.errors
-
         if (errors) {
-          setIsOpen(true)
-          setModalDescription(errors.join(', '))
-        } else {
-          const data = response
-          const updatedData = data.map(
-            (user: { profile_data: { profile_image: any } }) => {
-              if (user.profile_data?.profile_image) {
-                return {
-                  ...user,
-                  profile_data: {
-                    ...user.profile_data,
-                    profile_image: `${process.env.NEXT_PUBLIC_URL}${user.profile_data.profile_image}`
-                  }
-                }
-              }
-              return user
-            }
-          )
-          updatedData.sort(
-            (
-              a: {
-                profile_data: { first_name: { toLowerCase: () => number } }
-              },
-              b: { profile_data: { first_name: { toLowerCase: () => number } } }
-            ) => {
-              if (
-                a.profile_data?.first_name?.toLowerCase() <
-                b.profile_data?.first_name?.toLowerCase()
-              )
-                return -1
-              if (
-                a.profile_data?.first_name?.toLowerCase() >
-                b.profile_data?.first_name?.toLowerCase()
-              )
-                return 1
-              return 0
-            }
-          )
-          setUsersProfile(updatedData)
-          setModalDescription('')
+          return
         }
+        const data = response
+        const updatedData = data.map(
+          (user: { profile_data: { profile_image?: string } }) => ({
+            ...user,
+            profile_data: {
+              ...user.profile_data,
+              profile_image:
+                user.profile_data?.profile_image &&
+                `${process.env.NEXT_PUBLIC_URL}${user.profile_data.profile_image}`
+            }
+          })
+        )
+        updatedData.sort(
+          (
+            a: { profile_data: { first_name: { toLowerCase: () => number } } },
+            b: { profile_data: { first_name: { toLowerCase: () => number } } }
+          ) =>
+            a.profile_data?.first_name?.toLowerCase() <
+            b.profile_data?.first_name?.toLowerCase()
+              ? -1
+              : a.profile_data?.first_name?.toLowerCase() >
+                  b.profile_data?.first_name?.toLowerCase()
+                ? 1
+                : 0
+        )
+        setUsersProfile(updatedData)
       }
-      fetchProfileData()
     }
+    fetchData()
   }, [userId])
-
-  const handleCloseModal = () => {
-    setIsOpen(false)
-  }
 
   return (
     <div className="hidden px-6 py-6 md:block">
-      {isOpen && (
-        <ModalMessage
-          title="Erro"
-          description={modalDescription}
-          icon="critical"
-          onClose={handleCloseModal}
-        />
-      )}
-      {userId ? (
+      {usersProfile?.length ? (
         <InfoBlock title="Quem seguir">
-          {usersProfile?.map(
-            (user) =>
-              user.profile_data && (
-                <UserCard
-                  key={user.id}
-                  id={user.id}
-                  image={user?.profile_data?.profile_image || defaultUser}
-                  alt="imagem do usuário"
-                  username={
-                    user?.profile_data?.first_name +
-                      ' ' +
-                      user?.profile_data?.last_name || user?.username
-                  }
-                  follow
-                />
-              )
+          {usersProfile?.map((user) =>
+            user.profile_data ? (
+              <UserCard
+                key={user.id}
+                id={user.id}
+                image={user?.profile_data?.profile_image || defaultUser}
+                alt="imagem do usuário"
+                username={
+                  user?.profile_data?.first_name +
+                    ' ' +
+                    user?.profile_data?.last_name || user?.username
+                }
+                follow
+              />
+            ) : null
           )}
         </InfoBlock>
       ) : (
