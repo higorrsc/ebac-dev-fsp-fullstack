312b0b58c6c3d8804778ec3c3464d2c3a2f2fc95
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-20 22:24:29 -0300

    feat: add environment variables to settings

--- backend/social_h/settings.py
+++ backend/social_h/settings.py
@@ -30,10 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "1"
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(sep=" ")
 
 # Application definition
 
@@ -182,9 +181,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
