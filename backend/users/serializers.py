7d5b6c33d2505a753abc2e352f0b3bb88103ecec
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-20 22:25:43 -0300

    feat: add first and last name to user serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -9,14 +9,30 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("id", "username", "email", "is_active", "password", "profile_data")
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "is_active",
+            "password",
+            "profile_data",
+        )
         extra_kwargs = {
+            "first_name": {"required": True},
+            "last_name": {"required": True},
             "email": {"required": True},
             "password": {"write_only": True},
         }
 
     def create(self, validated_data):
-        user = User(email=validated_data["email"], username=validated_data["username"])
+        user = User(
+            email=validated_data["email"],
+            username=validated_data["username"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+        )
         user.set_password(validated_data["password"])
         user.save()
         return user
