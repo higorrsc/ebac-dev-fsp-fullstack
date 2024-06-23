3fbbfd8e443e55a260078fcc070db7918a31ad5c
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-23 00:11:47 -0300

    feat: add post query by owner

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -15,3 +15,10 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        queryset = Post.objects.all().order_by("id")
+        owner = self.request.query_params.get("owner")
+        if owner:
+            queryset = queryset.filter(owner_id=owner)
+        return queryset
