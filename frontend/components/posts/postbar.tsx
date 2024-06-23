f7314f605b036c0ed299ad573063d013ceceda3e
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-23 00:15:09 -0300

    feat: add fetch posts to postbar component

--- frontend/components/posts/postbar.tsx
+++ frontend/components/posts/postbar.tsx
@@ -1,58 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+import apiService from '@/app/services/apiService'
+import ModalMessage from '@/components/modalmessage'
 import { Post } from '@/components/posts/post'
 import { Post as PostType } from '@/lib/types'
 
-export default function PostBar() {
-  const posts = [
-    {
-      id: 1,
-      username: 'John Doe',
-      userId: 1,
-      profilePic: 'https://i.pravatar.cc/300',
-      image: 'https://picsum.photos/1920/1080',
-      content:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, nostrum.',
-      date: '2024-06-05',
-      category: '#post #python #javascript'
-    },
-    {
-      id: 2,
-      username: 'Jane Doe',
-      userId: 2,
-      profilePic: 'https://i.pravatar.cc/300',
-      content:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, nostrum.',
-      date: '2024-06-05',
-      category: '#post #python #nextjs'
-    },
-    {
-      id: 3,
-      username: 'John Doe',
-      userId: 1,
-      profilePic: 'https://i.pravatar.cc/300',
-      image: 'https://picsum.photos/1920/1080',
-      content:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, nostrum.',
-      date: '2024-06-05',
-      category: '#post #python #javascript'
-    },
-    {
-      id: 4,
-      username: 'Jane Doe',
-      userId: 2,
-      profilePic: 'https://i.pravatar.cc/300',
-      content:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, nostrum.',
-      date: '2024-06-05',
-      category: '#post #python #nextjs'
+type PostBarProps = {
+  userId?: number
+}
+
+export default function PostBar({ userId }: PostBarProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [modalDescription, setModalDescription] = useState('')
+  const [posts, setPosts] = useState<PostType[]>([])
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false)
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await apiService.get(`/posts/`)
+      const errors = response.errors
+      setModalDescription('')
+
+      if (errors) {
+        setModalDescription(errors.join(', '))
+      } else {
+        const data = response
+        setPosts(data)
+      }
     }
-  ]
+    fetchPosts()
+  }, [userId])
+
   return (
-    <div className="flex w-full flex-col gap-4">
-      {posts.map((post) => (
-        <Post key={post.id} {...post} />
-      ))}
-    </div>
+    <>
+      {isModalOpen && (
+        <ModalMessage
+          title="Erro"
+          description={modalDescription}
+          icon="critical"
+          onClose={handleCloseModal}
+        />
+      )}
+      <div className="flex w-full flex-col gap-4">
+        {posts &&
+          posts.map(
+            (post) =>
+              (!userId || userId == post.owner) && (
+                <Post key={post.id} post={post} />
+              )
+          )}
+      </div>
+    </>
   )
 }
