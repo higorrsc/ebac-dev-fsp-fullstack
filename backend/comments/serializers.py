9c6a5c9628af597ac11458a1ef27c1e018e65323
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-06-23 00:11:14 -0300

    feat: add owner field to comment serializer

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -5,6 +5,7 @@ from .models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
     commented_by = serializers.ReadOnlyField(source="owner.username")
+    owner = serializers.ReadOnlyField(source="owner.id")
 
     class Meta:
         model = Comment
@@ -13,6 +14,7 @@ class CommentSerializer(serializers.ModelSerializer):
             "comment",
             "comment_image",
             "comment_date",
+            "owner",
             "commented_by",
             "post",
         )
