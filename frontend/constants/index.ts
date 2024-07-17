7ad6918b0ef2c94984486b01c928cec3f009c3f4
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-07-16 21:29:54 -0300

    feat: add constants file

---
+++ frontend/constants/index.ts
@@ -0,0 +1,4 @@
+export const ACCESS_TOKEN_NAME = 'session_access_token'
+export const REFRESH_TOKEN_NAME = 'session_refresh_token'
+export const TOKEN_LIFE_LONG = 60 * 60 * 24 * 7
+export const TOKEN_LIFE_SHORT = 60 * 60
