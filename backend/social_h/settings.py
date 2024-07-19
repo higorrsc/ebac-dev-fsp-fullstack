503859fb44044d1dd2e04db545b63a0b6d496e8f
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-07-19 00:32:46 -0300

    feat: change tokens lifetime

--- backend/social_h/settings.py
+++ backend/social_h/settings.py
@@ -82,8 +82,8 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
